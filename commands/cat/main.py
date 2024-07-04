f90ab5e0e606ea0d5975b448c54bea1ca8f9b204
nabilasherif <nabila.sherif81@gmail.com>
2024-07-04 22:57:37 +0300

    added tree

--- commands/cat/main.py
+++ commands/cat/main.py
@@ -4,11 +4,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('filepath', action="store")
 parser.add_argument('-n',action='store_true',default= False)
 args = parser.parse_args()
-print(args)
+
 filepath = args.filepath
 f=open(filepath,"r")
 data=f.read()
 splitedData=data.split("\n")
+
 i=0
 while i< len(splitedData):
     if(hasattr(args,'n')):
--- commands/echo/main.py
+++ commands/echo/main.py
@@ -4,8 +4,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('toPrint', action="extend", nargs="+", type=str)
 parser.add_argument('-n',action='store_true',default= False)
 args = parser.parse_args()
+
 showNewLine=args.n
 tobePrinted=args.toPrint
+
 # print(" ".join(tobePrinted), end="")
 for word in tobePrinted:
     print(word, " ", end="")
